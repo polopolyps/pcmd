a7d709cb6cbee1e0cbdee2c36c3c921c3614891a
Andreas Ehrencrona <andreas.ehrencrona@atex.com>
2010-04-22 06:34:54 +0000

    various fixes...

--- branches/9.15-DR2-r55788/src/main/java/com/polopoly/util/policy/PolicyModification.java
+++ branches/9.15-DR2-r55788/src/main/java/com/polopoly/util/policy/PolicyModification.java
@@ -3,5 +3,5 @@ package com.polopoly.util.policy;
 import com.polopoly.cm.client.CMException;
 
 public interface PolicyModification<T> {
-	void modify(T newVersion) throws CMException;
+    void modify(T newVersion) throws CMException;
 }
