ae8ba574c5745ffdccb5456fc3e7aba6a975add8
Andreas Ehrencrona <andreas.ehrencrona@atex.com>
2011-07-01 14:07:03 +0000

    added missing Tool class

--- trunk/src/main/java/com/polopoly/pcmd/tool/Tool.java
+++ trunk/src/main/java/com/polopoly/pcmd/tool/Tool.java
@@ -3,16 +3,11 @@ package com.polopoly.pcmd.tool;
 import com.polopoly.ps.pcmd.FatalToolException;
 import com.polopoly.ps.pcmd.argument.Parameters;
 import com.polopoly.util.client.PolopolyContext;
-/**
- * Please do not move this interface, otherwise HelpTool wont work
- * @author andrew
- *
- * @param <P>
- */
+
 public interface Tool<P extends Parameters> {
-    void execute(PolopolyContext context, P parameters) throws FatalToolException;
+	void execute(PolopolyContext context, P parameters) throws FatalToolException;
 
-    P createParameters();
+	P createParameters();
 
-    String getHelp();
+	String getHelp();
 }
