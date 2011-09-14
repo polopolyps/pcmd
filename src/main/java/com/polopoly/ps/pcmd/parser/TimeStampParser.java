d733e0dd9c2b631c6c60832b71f1a03d7420eee5
Ubaldo Oloyola <ubaldo.oloyola@atex.com>
2011-09-14 12:26:23 +0000

    PSP-28 MultipleFileDeployer is not anymore awared of dryrun; Fixed errors in help test; Addd a TimeStampParser

--- src/main/java/com/polopoly/ps/pcmd/parser/TimeStampParser.java
+++ src/main/java/com/polopoly/ps/pcmd/parser/TimeStampParser.java
@@ -1,19 +1,19 @@
 package com.polopoly.ps.pcmd.parser;
 
-public class VersionParser implements Parser<Long>{
+public class TimeStampParser implements Parser<Long>{
 
     @Override
     public Long parse(String string) throws ParseException {
         try {
             return Long.parseLong(string);
         } catch (NumberFormatException e) {
-            throw new ParseException(this, string, "not a version");
+            throw new ParseException(this, string, "not a valid timestamp");
         }
     }
 
     @Override
     public String getHelp() {
-        return "<version>";
+        return "A timestamp as a long.";
     }
 
 }
