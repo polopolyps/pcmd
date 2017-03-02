7c0e152fc581543953cdb88d0adfbfb1b7b3c137
Marco Nova <mnova@atex.com>
2017-03-02 11:18:20 +0100

    When connecting to a polopoly version that does not have the HttpFileServiceClient do not complain about it and just print a warning.

--- pcmd/src/main/java/com/polopoly/pcmd/application/HttpFileServiceClientApplicationComponentProvider.java
+++ pcmd/src/main/java/com/polopoly/pcmd/application/HttpFileServiceClientApplicationComponentProvider.java
@@ -1,5 +1,8 @@
 package com.polopoly.pcmd.application;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import com.polopoly.application.Application;
 import com.polopoly.application.IllegalApplicationStateException;
 import com.polopoly.cm.client.HttpFileServiceClient;
@@ -7,9 +10,15 @@ import com.polopoly.ps.pcmd.ApplicationComponentProvider;
 
 public class HttpFileServiceClientApplicationComponentProvider implements ApplicationComponentProvider {
 
+	private static final Logger LOGGER = Logger.getLogger(HttpFileServiceClientApplicationComponentProvider.class.getName());
+
 	@Override
 	public void add(Application app) throws IllegalApplicationStateException {
-		app.addApplicationComponent(new HttpFileServiceClient());
+		try {
+			app.addApplicationComponent(new HttpFileServiceClient());
+		} catch (NoClassDefFoundError e) {
+			LOGGER.log(Level.WARNING, "A class is missing: " + e.getMessage());
+		}
 	}
 
 	@Override
